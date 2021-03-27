14964c39b936afc979fadfa95521a3be5a0e4bce
Ben <wrekone@users.noreply.github.com>
2021-03-27 10:22:52 -0700

    separate concerns & standardize

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ import os
 #from pprint import pprint
 
 API_KEY = os.environ.get('OPEN_WEATHER_MAPS_API_KEY')
+units = 'imperial'
 
-def get_city_weather():
+def select_location():
+  location = input("Enter a location : ")
+  return location
+
+def get_location_weather(location):
     if API_KEY:
-        city = input("Enter a city: ")
-        base_url = 'http://api.openweathermap.org/data/2.5/weather?appid=' + API_KEY + '&q=' + city
+        base_url = 'http://api.openweathermap.org/data/2.5/weather?appid=' + API_KEY + '&q=' + location + '&units=' + units
         weather_data = requests.get(base_url).json()
-        print(f"In {city}, the current weather is {weather_data['weather'][0]['description']}, with a relative humidity level of {weather_data['main']['humidity']}% and wind speeds of {weather_data['wind']['speed']} kmph.")
+        return weather_data
     else:
         print("Sorry, we seem to be having issues retrieving this data. Please try again later.")
 
-get_city_weather()
+location = select_location()
+weather = get_location_weather(location)
+
+print(f"In {location.title()}, the current weather is {weather['weather'][0]['description']}. The temperature is {round(weather['main']['temp'])}°F with a relative humidity level of {weather['main']['humidity']}% and wind speeds of {round(weather['wind']['speed'])} mph.")
+
+#pprint(weather)
